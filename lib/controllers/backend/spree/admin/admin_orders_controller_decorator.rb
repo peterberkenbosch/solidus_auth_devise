9a8eb619e275d4ed5b3855a1e7b856b48a5a3daf
Peter Berkenbosch <peter@pero-ict.nl>
2016-08-21 14:59:22 +0200

    before_filter is deprecated, updating all instances to before_action

--- lib/controllers/backend/spree/admin/admin_orders_controller_decorator.rb
+++ lib/controllers/backend/spree/admin/admin_orders_controller_decorator.rb
@@ -1,11 +1,11 @@
 Spree::Admin::OrdersController.class_eval do
-  before_filter :check_authorization
+  before_action :check_authorization
 
   private
     def load_order_action
       [:edit, :update, :cancel, :resume, :approve, :resend, :open_adjustments, :close_adjustments, :cart]
     end
-  
+
     def check_authorization
       action = params[:action].to_sym
       if load_order_action.include?(action)
